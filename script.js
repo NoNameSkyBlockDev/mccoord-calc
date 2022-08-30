69ebbf997165d31d67150f9685a8e7ffe2d8e499
sysnote8main <sysnoteser@gmail.com>
2022-08-30 20:58:03 +0900

    Update Files

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-  const selbox_world = document.getElementById("now_world");
+  const chkbox_world = document.getElementById("btn_toggle");
   const output_world = document.getElementById("output_world");
   const input_x = document.getElementById("coord_inx");
   const input_y = document.getElementById("coord_iny");
@@ -9,15 +9,15 @@ window.onload = function(){
   const output_z = document.getElementById("coord_outz");
   let coords = [];
   let output_coords = [];
-  let world_now = "Overworld";
+  let isnether = false;
   function get_nowworld() {
-    world_now = selbox_world.value;
-    if(world_now == "Overworld") {output_world.innerText = "Nether";}else {output_world.innerText = "Overworld";};
+    isnether = chkbox_world.checked;
+    if(isnether == false) {output_world.innerText = "Nether";}else {output_world.innerText = "Overworld";};
     calc_coords()
   };
   function calc_coords() {
     coords = [Number(input_x.value),Number(input_y.value),Number(input_z.value)];
-    if(world_now == "Overworld") {
+    if(isnether == false) {
       output_coords = coords.map(x => Math.floor(x/8));
       output_coords[1] = coords[1];
     }else {
@@ -28,7 +28,7 @@ window.onload = function(){
     output_y.value = output_coords[1];
     output_z.value = output_coords[2];
   }
-  selbox_world.addEventListener("change",get_nowworld);
+  chkbox_world.addEventListener("change",get_nowworld);
   input_x.addEventListener("change",calc_coords);
   input_y.addEventListener("change",calc_coords);
   input_z.addEventListener("change",calc_coords);
